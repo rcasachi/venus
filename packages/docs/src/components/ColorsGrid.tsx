217c0af6c9689f8adf5133e4fc0a1f28b6cb2794
rafael.casachi <rcasachi@gmail.com>
2022-12-27 16:26:47 -0300

    feat(docs): add pages for each token sets

--- packages/docs/src/components/ColorsGrid.tsx
+++ packages/docs/src/components/ColorsGrid.tsx
@@ -1,28 +1,21 @@
-// import { Box } from '@venusui/components'
-// import { LightThemeColors, DarkThemeColors } from '@venusui/tokens'
-// import { getContrast } from 'polished'
+import { Text, Flex, Grid, Box } from '@venusui/components'
+import { LightThemeColors, DarkThemeColors } from '@venusui/tokens'
 
-// const lightColors = { ...LightThemeColors.tone, ...LightThemeColors.colors }
-// const darkColors = { ...DarkThemeColors.tone, ...DarkThemeColors.colors }
-// const colors = { ...lightColors, ...darkColors }
+const lightColors = { ...LightThemeColors.tone, ...LightThemeColors.colors }
+const darkColors = { ...DarkThemeColors.tone, ...DarkThemeColors.colors }
+const colors = { ...lightColors, ...darkColors }
 
-// export function ColorsGrid() {
-//   return Object.entries({ colors }).map(([key, color]) => {
-//     return (
-//       <Box key={key} css={{ backgroundColor: color, padding: '2rem' }}>
-//         <div
-//           style={{
-//             display: 'flex',
-//             justifyContent: 'space-between',
-//             fontFamily: 'monospace',
-//             color: getContrast(color, '#FFF') < 3.5 ? '#000' : '#FFF',
-//           }}
-//         >
-//           <strong>${key}</strong>
-//           <span>{color}</span>
-//         </div>
-//       </Box>
-//     )
-//   })
-// }
-export {}
+export function ColorsGrid() {
+  return (
+    <Grid columns="10" align="center" gapX="3" gapY="6">
+      {Object.entries(colors).map(([key, color]) => {
+        return (
+          <Flex direction="column">
+            <Box key={key} css={{ backgroundColor: color, padding: '2rem' }} />
+            <Text css={{fontWeight: '$3', display: 'block'}} variant="yellow" as="div">${key}</Text>
+          </Flex>
+        )
+      })}
+    </Grid>
+  )
+}
--- packages/docs/src/components/TokensGrid.tsx
+++ packages/docs/src/components/TokensGrid.tsx
@@ -1,35 +1,39 @@
-// import '../styles/tokens-grid.css'
+import { Text } from '@venusui/components'
+import '../styles/tokens-grid.css'
 
-// interface TokensGridProps {
-//   tokens: Record<string, string>
-//   hasRemValue?: boolean
-// }
+interface TokensGridProps {
+  tokens: Record<string, string>
+  hasRemValue?: boolean
+}
 
-// export function TokensGrid({ tokens, hasRemValue = false }: TokensGridProps) {
-//   return (
-//     <table className="tokens-grid">
-//       <thead>
-//         <tr>
-//           <th>Name</th>
-//           <th>Value</th>
-//           {hasRemValue && <th>Pixels</th>}
-//         </tr>
-//       </thead>
+export function TokensGrid({ tokens, hasRemValue = false }: TokensGridProps) {
+  if (Object.entries(tokens).length === 0) {
+    return <Text variant="gold">No tokens at this section</Text>
+  }
 
-//       <tbody>
-//         {Object.entries(tokens).map(([key, value]) => {
-//           return (
-//             <tr key={key}>
-//               <td>{key}</td>
-//               <td>{value}</td>
-//               {hasRemValue && (
-//                 <td>{Number(value.replace('rem', '')) * 16}px</td>
-//               )}
-//             </tr>
-//           )
-//         })}
-//       </tbody>
-//     </table>
-//   )
-// }
-export {}
+  return (
+    <table className="tokens-grid">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Value</th>
+          {hasRemValue && <th>Pixels</th>}
+        </tr>
+      </thead>
+
+      <tbody>
+        {Object.entries(tokens).map(([key, value]) => {
+          return (
+            <tr key={key}>
+              <td>${key}</td>
+              <td>{value}</td>
+              {hasRemValue && (
+                <td>{Number(value.replace('rem', '')) * 16}px</td>
+              )}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
+}
