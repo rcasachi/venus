c878f55609f07ce909a7064379d45521aff78a30
rcasachi <rcasachi@gmail.com>
2023-02-15 12:03:50 -0300

    feat(docs): improve alert docs

---
+++ packages/docs/src/stories/display/alert.stories.tsx
@@ -0,0 +1,58 @@
+import { Alert, AlertProps, Heading, Link, Text } from '@venusui/components'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Display/Alert',
+  component: Alert,
+  args: {
+    variant: 'contrast',
+    size: 1,
+    children: (
+      <Text size="2" variant="gray">
+        A modal dialog that interrupts the user with{' '}
+        <Link href="#">important content</Link> and expects a response.
+      </Text>
+    ),
+  },
+  argTypes: {
+    variant: {
+      options: ['contrast', 'gray', 'red', 'blue', 'green'],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+    size: {
+      options: [1, 2, 3],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+    children: {
+      control: {
+        type: null,
+      },
+    },
+  },
+} as Meta<AlertProps>
+
+export const Default: StoryObj<AlertProps> = {}
+
+export const WithHeading: StoryObj<AlertProps> = {
+  args: {
+    variant: 'contrast',
+    children: (
+      <>
+        <Heading
+          size="3"
+          css={{ fontWeight: 500, lineHeight: '20px', marginBottom: '$1' }}
+        >
+          Alert heading
+        </Heading>
+        <Text size="2" variant="gray" css={{ lineHeight: '17px' }}>
+          A modal dialog that interrupts the user with{' '}
+          <Link href="#">important content</Link> and expects a response.
+        </Text>
+      </>
+    ),
+  },
+}
