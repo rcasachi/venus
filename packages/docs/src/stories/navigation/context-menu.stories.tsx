ab8b97def734419b579104fabf9212820570878d
rcasachi <rcasachi@gmail.com>
2023-03-08 12:55:19 -0300

    feat(docs): improve contextmenu docs

---
+++ packages/docs/src/stories/navigation/context-menu.stories.tsx
@@ -0,0 +1,111 @@
+import {
+  ChevronRightIcon,
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuPortal,
+  ContextMenuProps,
+  ContextMenuSeparator,
+  ContextMenuSub,
+  ContextMenuSubContent,
+  ContextMenuSubTrigger,
+  ContextMenuTrigger,
+  ContextMenuCheckboxItem,
+  ContextMenuItemIndicator,
+  ContextMenuLabel,
+  ContextMenuRadioGroup,
+  ContextMenuRadioItem,
+  RightSlot,
+  Flex,
+  Text
+} from '@venusui/components'
+import type { Meta, StoryObj } from '@storybook/react'
+import { useState } from 'react';
+
+export default {
+  title: 'Navigation/ContextMenu',
+} as Meta<ContextMenuProps>
+
+export const Default: StoryObj<ContextMenuProps> = {
+  render: () => {
+    const [bookmarksChecked, setBookmarksChecked] = useState(true);
+    const [urlsChecked, setUrlsChecked] = useState(false);
+    const [person, setPerson] = useState('pedro');
+
+    return (
+      <ContextMenu>
+        <ContextMenuTrigger asChild>
+          <Flex
+            css={{
+              alignItems: 'center',
+              justifyContent: 'center',
+              height: '$12',
+              backgroundColor: '$slate2',
+              border: '2px dashed $colors$slate6',
+              borderRadius: '$2',
+            }}
+          >
+            <Text variant="gray">Right-click me</Text>
+          </Flex>
+        </ContextMenuTrigger>
+        <ContextMenuPortal>
+          <ContextMenuContent sideOffset={5} align="end">
+            <ContextMenuItem>
+              Back <RightSlot>⌘+[</RightSlot>
+            </ContextMenuItem>
+            <ContextMenuItem disabled>
+              Foward <RightSlot>⌘+]</RightSlot>
+            </ContextMenuItem>
+            <ContextMenuItem>
+              Reload <RightSlot>⌘+R</RightSlot>
+            </ContextMenuItem>
+            <ContextMenuSub>
+              <ContextMenuSubTrigger>
+                More Tools
+                <RightSlot>
+                  <ChevronRightIcon />
+                </RightSlot>
+              </ContextMenuSubTrigger>
+              <ContextMenuPortal>
+                <ContextMenuSubContent sideOffset={2} alignOffset={-5}>
+                  <ContextMenuItem>
+                    Save Page As… <RightSlot>⌘+S</RightSlot>
+                  </ContextMenuItem>
+                  <ContextMenuItem>Create Shortcut…</ContextMenuItem>
+                  <ContextMenuItem>Name Window…</ContextMenuItem>
+                  <ContextMenuSeparator />
+                  <ContextMenuItem>Developer Tools</ContextMenuItem>
+                </ContextMenuSubContent>
+              </ContextMenuPortal>
+            </ContextMenuSub>
+
+            <ContextMenuSeparator />
+
+            <ContextMenuCheckboxItem checked={bookmarksChecked} onCheckedChange={setBookmarksChecked}>
+              <ContextMenuItemIndicator />
+              Show Bookmarks <RightSlot>⌘+B</RightSlot>
+            </ContextMenuCheckboxItem>
+            <ContextMenuCheckboxItem checked={urlsChecked} onCheckedChange={setUrlsChecked}>
+              <ContextMenuItemIndicator />
+              Show Full URLs
+            </ContextMenuCheckboxItem>
+
+            <ContextMenuSeparator />
+
+            <ContextMenuLabel>People</ContextMenuLabel>
+            <ContextMenuRadioGroup value={person} onValueChange={setPerson}>
+              <ContextMenuRadioItem value="pedro">
+                <ContextMenuItemIndicator />
+                Pedro Duarte
+              </ContextMenuRadioItem>
+              <ContextMenuRadioItem value="colm">
+                <ContextMenuItemIndicator />
+                Colm Tuite
+              </ContextMenuRadioItem>
+            </ContextMenuRadioGroup>
+          </ContextMenuContent>
+        </ContextMenuPortal>
+      </ContextMenu>
+    )
+  },
+}
